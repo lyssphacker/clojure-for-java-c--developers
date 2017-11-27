95ffe439660950fa84f6e46d55bf3f134a17d2c6
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-11-27 05:57:28 +0100

    Trying to have multiple arguments.

--- clojure-example/src/clojure_example/example.clj
+++ clojure-example/src/clojure_example/example.clj
@@ -1,14 +1,15 @@
 (ns clojure-example.example)
 
-(defmulti parse-line (fn [x] x))
+(defmulti parse-line (fn [x y] x))
 
 (defmacro defmapping [name & fields]
-  `(defmethod parse-line ~name [x#]
+  `(defmethod parse-line [~name String] [x# line#]
      (for [el# '~fields]
        (let [[start# end# property#]
              el#]
          (println start#)
          (println end#)
          (println property#)
-         ))))
+         ))
+     line#))
 
